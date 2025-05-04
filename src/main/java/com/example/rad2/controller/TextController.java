46715b013c574987e347eb7d30e5c171a7e28add
Alexsandrt <ceberganov2@gmail.com>
2025-05-04 23:35:40 +0400

    jjy

--- src/main/java/com/example/rad2/controller/TextController.java
+++ src/main/java/com/example/rad2/controller/TextController.java
@@ -2,6 +2,8 @@ package com.example.rad2.controller;
 
 import com.example.rad2.service.TextService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,28 @@ public class TextController {
     private TextService textService;
 
     @PostMapping("/texts/add")
+    public ResponseEntity<String> addText(@RequestParam("text") String text) {
+        try {
+            textService.addText(text);
+            return ResponseEntity.ok("Благодарим за ваше дополнение, после проверки ваш текст будет добавлен на сайт");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка на стороне сервера");
+        }
+    }
+}
+
+
+
+/*@RestController
+public class TextController {
+
+    @Autowired
+    private TextService textService;
+
+    @PostMapping("/texts/add")
     public String addText(@RequestParam("text") String text) {
         textService.addText(text);
         return "Благодарим за ваше дополнение, после проверки ваш текст будет добавлен на сайт";
     }
-}
+}*/
