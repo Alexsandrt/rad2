5933a9415b8d3cfb0814b8ff9502a710ef72b424
Alexsandrt <ceberganov2@gmail.com>
2025-05-06 06:03:49 +0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/rad2/controller/TextController.java
+++ src/main/java/com/example/rad2/controller/TextController.java
@@ -1,3 +1,30 @@
+package com.example.rad2.controller;
+
+import com.example.rad2.service.TextService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TextController {
+
+    @Autowired
+    private TextService textService;
+
+    @PostMapping("/texts/add")
+    public ResponseEntity<String> addText(@RequestParam("text") String text) {
+        try {
+            textService.addText(text);
+            return ResponseEntity.ok("Благодарим за ваше дополнение, после проверки ваш текст будет добавлен на сайт");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка на стороне сервера");
+        }
+    }
+}
 
 
 
